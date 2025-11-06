f06cfc9188cbdf0a971c43037785d8d6ab7f3e24
VNOGITHUB <153068486+VNOGITHUB@users.noreply.github.com>
2025-11-06 06:23:29 +0530

    Add files via upload

---
+++ 1_Pratice_Print_function.py
@@ -0,0 +1,7 @@
+print("My parents is Shiva Parvathy")  # Using Double ("")Quote
+print('My parents is Shiva Parvathy')  # Using Single ('') Quote
+print("My parents is 'Shiva Parvathy' ")
+print('My parents is Shiva Parvathy')
+print('This book is vinoth\'s ')
+
+
